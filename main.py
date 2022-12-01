33daf3be216b08ab7602ce615611be6266f13177
Fuyao <fuyao@officenas.fastwise.net>
2022-12-01 10:45:39 +0800

    edit_on_branch

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
 
+def edit_function():
+    print("Hello")
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
